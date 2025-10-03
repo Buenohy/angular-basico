378eb520f99dadb26caa6579a68a680fdd3c534f
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-03 20:52:11 -0300

    fix: error in new sintaxe @if

--- src/app/componente04/componente04.ts
+++ src/app/componente04/componente04.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-componente04',
-  imports: [],
+  standalone: true,
   templateUrl: './componente04.html',
-  styleUrl: './componente04.css'
+  styleUrl: './componente04.css',
 })
 export class Componente04 {
-
+  // Variável para exibir ou ocultar o quadrado
+  exibir: boolean = true;
 }
