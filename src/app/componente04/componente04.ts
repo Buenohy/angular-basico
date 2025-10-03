cf1b7cd1f2209a5201a86e4cf411b1999ed46818
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-03 20:14:12 -0300

    feat: componente04

---
+++ src/app/componente04/componente04.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente04',
+  imports: [],
+  templateUrl: './componente04.html',
+  styleUrl: './componente04.css'
+})
+export class Componente04 {
+
+}
