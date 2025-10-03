31f1d147bece4540e0b2caac61773017bdf86058
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-03 09:35:17 -0300

    feat: text and account of the interpolation

--- src/app/componente01/componente01.ts
+++ src/app/componente01/componente01.ts
@@ -4,8 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-componente01',
   imports: [],
   templateUrl: './componente01.html',
-  styleUrl: './componente01.css'
+  styleUrl: './componente01.css',
 })
 export class Componente01 {
-
+  // Variáveis
+  nome: string = 'Gabriel';
+  idade: number = 23;
 }
